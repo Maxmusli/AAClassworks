e150aaa186eb26e8e1a84e1a70e073dbb68a414c
max <maxmus2223@gmail.com>
2019-10-24 08:52:41 -0700

    create and finished minesweeper game using React

---
+++ W10D3/minesweeper/src/board.jsx
@@ -0,0 +1,33 @@
+import Tile from './tile';
+import React, { Component } from 'react';
+// rcc tab to create import and class
+
+export default class board extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    console.log("board");
+    return (
+      <div className="board">
+        { this.props.board.grid.map((row, index) => {
+          return (
+            <div className="row" key={index}>
+              { row.map((tile, idx) => {
+                return (
+                  <Tile 
+                    key={idx} 
+                    tile={tile} 
+                    updateGame={this.props.updateGame}
+                  /> 
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
