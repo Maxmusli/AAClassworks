3e54b48a5a888c8fea2a7a36a276b4bf2aa7cff1
max <maxmus2223@gmail.com>
2019-11-16 17:35:32 -0800

    add user registration route

--- W14/MERN twitter/app.js
+++ W14/MERN twitter/app.js
@@ -5,13 +5,22 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "Max",
+    email: "max@max.com",
+    password: "password"
+  })
+  user.save()
+  res.send("Hello")
+});
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
